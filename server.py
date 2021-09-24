89a55ac2854ca18f47ece889738ca0cc9e41c3ff
dazhi-z <72182943+dazhi-z@users.noreply.github.com>
2021-09-24 13:13:13 -0600

    Update server.py
    Update server.py

--- server.py
+++ server.py
@@ -32,7 +32,75 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        #print ("Got a request of: %s\n" % self.data.decode("utf-8"))
+
+        split_data = self.data.decode("utf-8").split('\r\n')
+        first_line_request = split_data[0].split(' ')
+
+        
+        if (first_line_request[0] == "GET") :
+        #only view files
+            if_correct = 0
+            root = "www"
+            baseAddress = first_line_request[1]
+            url = root + baseAddress
+            baseURL = "http://127.0.0.1:8080" + baseAddress
+
+            if ( baseAddress[len(baseAddress)-1] == '/' ):
+                url = url + "index.html"
+
+            split_url = url.split(".")
+
+            if ( len(split_url) == 1 ):
+            #judge if an index.html file is contained under the directory
+                url = url + "/index.html"
+                BaseURL = baseURL + "/"
+                if_correct = 1
+
+            elif ( len(split_url) == 2 ):
+                #support css and html
+                if ( not ( (split_url[1] == "css") or (split_url[1] == "html") ) ):
+                    self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n",'utf-8'))
+                    return 0
+            else:
+                self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n",'utf-8'))
+                return 0
+
+            try:
+            #open return file
+                files = open(url, "r")
+            except:
+                self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n",'utf-8'))
+                return 0
+
+            content_type = "Content-Type: text/html\r\n"
+            if ( (url.split("."))[1] == "css" ):
+                content_type = "Content-Type: text/css\r\n"
+
+            
+            if (if_correct == 0):
+                self.request.sendall(bytearray("HTTP/1.1 200 OK\r\n",'utf-8'))
+            elif (if_correct == 1):
+            #if there is an inde.html under the directory add '/' to the end if it does not exists
+                #https://zh.wikipedia.org/wiki/HTTP_301 Viewed this page for how to format 301 status code
+                self.request.sendall(bytearray("HTTP/1.1 301 Moved Permanently\r\n",'utf-8'))
+                self.request.sendall(bytearray("Location: " + BaseURL + "\r\n",'utf-8'))
+            self.request.sendall(bytearray(content_type,'utf-8'))
+            
+            data = files.read(1024)
+            #view the file
+            while (data):
+                self.request.send( bytearray(data,'utf-8') )
+                data = files.read(1024)
+
+            files.close()
+            return 0
+        #405 when handle POST, PUT or DELETE requests
+        else:
+            self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed",'utf-8'))
+            return 0
+
+        #self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n",'utf-8'))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
